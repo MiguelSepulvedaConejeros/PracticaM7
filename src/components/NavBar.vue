94d8ab732a6c108cfba523727c320e287ea6f430
MiguelSepulvedaConejeros <msepco@gmail.com>
2025-01-12 19:39:38 -0300

    finalizando detalle

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,37 @@
 <template>
     <v-app-bar color="#6200EA">
-      <v-menu open-on-hover>
-        <template v-slot:activator="{ props }">
-          <span v-bind="props"><v-app-bar-nav-icon></v-app-bar-nav-icon></span>
-        </template>
-        <v-list>
-          <v-list-item v-for="(item, index) in items" :key="index">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-toolbar-title>Cursos AlfaWeb</v-toolbar-title>
-      <router-link to="/"><v-icon color="white" type="button" class="mr-5">mdi-home</v-icon></router-link>
-      <router-link to="/adminview"><v-icon color="white" type="button" class="mr-5">mdi-wrench</v-icon></router-link>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <span class="text-h6 ml-2">Cursos AlfaWeb</span>
+      <v-spacer></v-spacer>
+      <v-icon @click="$router.push('/')" color="white" type="button" class="mr-5">mdi-home</v-icon>
+      <v-icon @click="$router.push('/adminview')" color="white" type="button" class="mr-5">mdi-wrench</v-icon>
     </v-app-bar>
+
+    <v-navigation-drawer color="deep-purple-lighten-2" v-model="drawer" app temporary>
+      <v-list>
+        <v-list-item-group>
+
+          <v-list-item @click="$router.push('/')">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title> Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item @click="$router.push('/adminview')">
+            <v-list-item-icon>
+              <v-icon>mdi-wrench</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>  Administracion</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 </template>
 
 <script>
@@ -21,16 +39,18 @@
     name: 'NavBar',
     data() {
       return {
-        items: [
-          { title: 'uno'},
-          { title: 'dos'},
-          { title: 'tres'},
-          { title: 'cuatro'},
-        ],
+        drawer: false,
       }
     },
   }
 </script>
 
 <style scoped>
+.v-list-item {
+  cursor: pointer;
+}
+.v-list-item-title{
+  display: inline;
+  color: white;
+}
 </style>
